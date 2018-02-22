37b3fd8d01828335a877738c7766441f0500cf0c
Efrain Perez Jr <efrainperezjr@live.com>
2018-02-22 20:47:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,13 +9,18 @@ function currentLine(deliLine) {
   } else {
     var line = `The line is currently: 1. ${deliLine[0]}`
     for (var i = 2; i <= deliLine.length; i++) {
-      line = line + `, ${i}. ${deliLine[i-1]}` 
+      line = line + `, ${i}. ${deliLine[i-1]}`
     }
     return line
   }
 }
 
 function nowServing(deliLine) {
-  var name = deliLine.shift()
-  return `Currently serving ${name}.`
+  var name
+  if (deliLine.length == 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    name = deliLine.shift()
+    return `Currently serving ${name}.`
+  }
 }
